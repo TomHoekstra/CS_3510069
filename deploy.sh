0b036cdfc9ced21afddae178dfa6bf96641dd198
Tom Hoekstra <Tom-1509@hotmail.com>
2017-10-10 17:44:18 +0200

    fix deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,6 @@
 if [ -e "$DEPLOYMENT_TARGET/package.json" ]; then
   cd "$DEPLOYMENT_TARGET"
-  eval $NPM_CMD install --production
-  eval $NPM_CMD install --only=dev
+  eval $NPM_CMD install
   eval $NPM_CMD start
   exitWithMessageOnError "npm failed"
   cd - > /dev/null
